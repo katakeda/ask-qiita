1b9f180c0278c623463dc6fbda48cf78aa7e6981
Kaitaku Takeda <katakeda32@gmail.com>
2019-02-09 02:06:56 -0800

    Copy files

---
+++ spec/ask-qiita-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AskQiitaView from '../lib/ask-qiita-view';
+
+describe('AskQiitaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
